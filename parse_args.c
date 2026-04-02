d03755cfd7d3282c0f5d53c6a4941d3864fc22f7
Tiana Ismael Kamil Houssen <tkamil-h@student.42antananarivo.mg>
2026-04-02 14:46:32 +0300

    add file all

--- parse_args.c
+++ parse_args.c
@@ -6,7 +6,7 @@
 /*   By: tkamil-h <tkamil-h@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 14:36:05 by tkamil-h          #+#    #+#             */
-/*   Updated: 2026/04/02 10:34:46 by tkamil-h         ###   ########.fr       */
+/*   Updated: 2026/04/02 14:07:33 by tkamil-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,24 @@ static void	append_to_stack(t_info *info, int value)
 	ft_lstadd_back_ps(&info->stack_a, new);
 }
 
-void            parse_args(t_info *info, int argc, char **argv)
+void	parse_args(t_info *info, int argc, char **argv)
 {
-    int     i;
-    long    val;
+	int		i;
+	long	val;
 
-    i = 1;
-    while (i < argc)
-    {
-        if (!is_valid_number(argv[i]))
-            error_exit();
-        val = ft_atol(argv[i]);
-        if (val < INT_MIN || val > INT_MAX)
-            error_exit();
-        if (has_duplicate(info->stack_a, (int)val))
-            error_exit();
-        append_to_stack(info, (int)val);
-        info->size_a++;
-        i++;
-    }
+	i = 0;
+	while (i < argc)
+	{
+		if (!is_valid_number(argv[i]))
+			error_exit();
+		val = ft_atol(argv[i]);
+		if (val < INT_MIN || val > INT_MAX)
+			error_exit();
+		if (has_duplicate(info->stack_a, (int)val))
+			error_exit();
+		append_to_stack(info, (int)val);
+		info->size_a++;
+		i++;
+	}
 }
 
